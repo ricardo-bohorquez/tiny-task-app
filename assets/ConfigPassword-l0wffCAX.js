71dc94d45d3978df62ecd84d7c15881354c48aee
ricardo-bohorquez <ricardoj.dev@gmail.com>
2024-01-30 16:19:39 -0400

    Solving error in register user

---
+++ assets/ConfigPassword-l0wffCAX.js
@@ -0,0 +1,1 @@
+import{cf as s}from"./index-qVbPZHpg.js";function r(){return s.jsxs(s.Fragment,{children:[s.jsx("h2",{children:"Configura tu contraseña"}),s.jsx("h3",{children:"Estamos trabajando en esta sección"})]})}export{r as default};
--- assets/NotFound-1Jpi3bwC.js
+++
@@ -1,1 +0,0 @@
-import{cf as e}from"./index-tupzdFi5.js";function t(){return e.jsx("div",{children:e.jsx("h1",{children:"La página que busca no existe"})})}export{t as default};
