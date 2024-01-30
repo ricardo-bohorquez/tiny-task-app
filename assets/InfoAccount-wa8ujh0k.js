e89a9964c788cd0153a8594da23d94347f243da2
ricardo-bohorquez <ricardoj.dev@gmail.com>
2024-01-30 10:24:52 -0400

    upload changes

---
+++ assets/InfoAccount-wa8ujh0k.js
@@ -0,0 +1,1 @@
+import{ce as c,cf as n}from"./index-tupzdFi5.js";import{S as l}from"./Settings-hGI8SVM-.js";function u(){const{info:{accountCreationDate:o,loginWithGoogle:t}}=c(),{REG_DATE:s,REG_METHOD:e,TYPE_EMAIL:i,TYPE_GOOGLE:r}=l;return n.jsx(n.Fragment,{children:n.jsxs("ul",{children:[n.jsxs("li",{children:[s,": ",o]}),n.jsxs("li",{children:[e,": ",t?r:i]})]})})}export{u as default};
---
+++ assets/headerConstants-7m8t-61l.js
@@ -0,0 +1,1 @@
+const e=Object.freeze({SING_IN:"Iniciar sesión",SING_UP:"Registrate",LOG_OUT:"Cerrar sesión",APP_NAME:"Tiny Task"});export{e as H};
